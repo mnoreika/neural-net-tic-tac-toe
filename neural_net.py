237c06df3077d34f9f5b69eda41ae1337c66451b
mtnoreika <mn55@st-andrews.ac.uk>
2017-04-07 17:38:09 +0100

    Added biases to the network

--- neural_net.py
+++ neural_net.py
@@ -20,101 +20,99 @@ class Network():
 		O = []
 		layer_outputs = inputs
 
-		for bias, weight in zip(self.biases, self.weights):
-			layer_outputs = self.activation_function(numpy.dot(weight, layer_outputs))
+		for biases, weights in zip(self.biases, self.weights):
+		
+			weighted_sum = numpy.dot(weights, layer_outputs)
+			z = []
+
+			for i in range(len(weighted_sum)):
+				print (biases[i][0])
+				z.append(weighted_sum[i] + biases[i][0]) 
+
+			layer_outputs = self.activation_function(numpy.array(z))
+			# print (len(layer_outputs))
+			# print (layer_outputs)
+			# print ()
 			O.append(layer_outputs)
+
+			outputs = []
+			# for i in range(len(weights)):
+			# 	output = 0
+			# 	for j in range(len(weights[i])):
+			# 		output += weights[i][j] * layer_outputs[i]
+
+			# 	outputs.append(output)
+			# 	print (output)	
+
+			# print (outputs)	
+			# layer_outputs = outputs
+			# O.append(outputs)
+			
 
 		return O
 
 
 	def back_propagate(self, training_data):	
 
-		for inputed, expected in training_data:
-			for epoch in range(1000):
-				O = net.feedforward(inputed)
-				one_vector = [1] * len(O)
+		for epoch in range(1000):
+			for inputed, expected in training_data:
 				
-				# Input layer has no outputs
-				last_layer = self.number_of_layers - 2
-
-				# Updating weights of the output layer
-				delta_error_L = numpy.multiply(numpy.multiply(O[last_layer], 1 - O[last_layer]), O[last_layer] - expected)
-
-
-				for i in range(len(self.weights[last_layer]) - 1, -1, -1):
-					delta_weight = numpy.multiply(-1 * self.learning_rate,  numpy.multiply(delta_error_L[i], O[last_layer - 1]))
+					O = net.feedforward(inputed)
+					one_vector = [1] * len(O)
 					
-					for j in range(len(self.weights[last_layer][i])):
-						self.weights[last_layer][i][j] += delta_weight[j]
+					# Input layer has no outputs
+					last_layer = self.number_of_layers - 2
+
+					# Updating weights of the output layer
 				
-				
-				# Updating weights of the hidden layers
-				previous_delta_error = delta_error_L
-
-				for i in range(last_layer - 1, -1, -1):
-					delta_error_l = []
-
-					for j in range(len(self.weights[i])):
-						sum_of_L = 0 
-						print (i)
-						for k in range(len(self.weights[i + 1])):
-							sum_of_L += previous_delta_error[k] * self.weights[i + 1][k][j]
+					delta_error_L = numpy.multiply(numpy.multiply(O[last_layer], 1 - O[last_layer]), O[last_layer] - expected)
 
 
-						delta_error_l_neuron = O[i][j] * (1 - O[i][j]) * sum_of_L
-						delta_error_l.append(delta_error_l_neuron)
+					for i in range(len(self.weights[last_layer]) - 1, -1, -1):
+						delta_weight = numpy.multiply(-1 * self.learning_rate,  numpy.multiply(delta_error_L[i], O[last_layer - 1]))
+						delta_bias = -1 * self.learning_rate * delta_error_L[i]
 
-						delta_weight = numpy.multiply(-1 * self.learning_rate,  numpy.multiply(delta_error_l_neuron, O[i - 1]))
-						print(delta_weight)
-						# Updating the weights of a particular neuron
-						for w in range(len(self.weights[i][j])):
-							self.weights[i][j][w] += delta_weight[w]
+						print ("DELTA_SIGMA ", delta_bias)
 
+						for j in range(len(self.weights[last_layer][i])):
+							self.weights[last_layer][i][j] += delta_weight[j]
 
-						
-					previous_delta_error = delta_error_l
-
-				# break	
-					# print (previous_delta_error)
-					# print (weights[i])
-				# sum_of_L = 0 
-				# for k in range(len(previous_delta_error)):
-				# 	sum_of_L += numpy.dot(previous_delta_error[k], self.weights[i])
-
-
-			    # delta_error_l = numpy.multiply(numpy.multiply(O[i], 1 - O[i]), sum_of_L)
-			    # print (previous_delta_error_transposed)
-
-
-
-				# previous_delta_error = delta_error_l
-
-				# print (delta_error_l)
-
-
-				# for j in range(len(self.weights[i])):
-				# 	print()
-				# 	print (delta_error_l[j])
-				# 	print(O[last_layer - 2 - i])
-
-				# 	delta_weight = numpy.multiply(-1 * self.learning_rate,  numpy.multiply(delta_error_l[i][j], O[last_layer - 2 - i]))
-
-				# 	print (delta_weight)	
+						# Updating the bias 
+						self.biases[last_layer][i] += delta_bias
 					
-				# 	# Calculating the delta
-				# 	for n in range(len(self.weights[i][j])):
-						
+					
+					# Updating weights of the hidden layers
+					previous_delta_error = delta_error_L
 
-				# 		print (delta_weight)	
+					for i in range(last_layer - 1, -1, -1):
+						delta_error_l = []
 
-				# 		for w in range(len(self.weights[i][j])):
-				# 			print (self.weights[i][j][n])
+						for j in range(len(self.weights[i])):
+							sum_of_L = 0 
+							
+							for k in range(len(self.weights[i + 1])):
+								sum_of_L += previous_delta_error[k] * self.weights[i + 1][k][j]
 
-				# 		self.weights[i][j][w] += delta_weight[w]
-				# 		print (self.weights[i][j][w])
-				
 
-				
+							delta_error_l_neuron = O[i][j] * (1 - O[i][j]) * sum_of_L
+							delta_error_l.append(delta_error_l_neuron)
+
+							delta_weight = numpy.multiply(-1 * self.learning_rate,  numpy.multiply(delta_error_l_neuron, O[i - 1]))
+
+							delta_bias = -1 * self.learning_rate * delta_error_l_neuron
+							
+							# print(delta_weight)
+							# Updating the weights of a particular neuron
+							for w in range(len(self.weights[i][j])):
+								self.weights[i][j][w] += delta_weight[w]
+
+							# Updating the bias 	
+							self.biases[i][j] += delta_bias	
+
+
+							
+						previous_delta_error = delta_error_l
+					
 
 					
 				
@@ -122,15 +120,20 @@ class Network():
 
 
 
-net = Network([3, 9, 1, 3])
+net = Network([9, 9, 9, 9])
 
 # inputs = [2, 3]		
 
 
 # # net.feedforward(inputs)
-net.back_propagate([([0, 0, 0], [0.5, 0.5, 0.5])]);
+net.back_propagate([([0, 0, 0, 0, 0, 0, 0, 0, 0], [1, 1, 1, 1, 1, 1, 1, 1, 1]), ([1, 1, 1, 1, 1, 1, 1, 1, 1], [0, 0, 0, 0, 0, 0, 0, 0, 0])]);
 
-result = net.feedforward([0, 0, 0])
 
-print (result)
-# # net.learn([(5, 0)])
+result = net.feedforward([1, 1, 1, 1, 1, 1, 1, 1, 1])
+print ("Result 1: ", result[net.number_of_layers - 2])
+result = net.feedforward([0, 0, 0, 0, 0, 0, 0, 0, 0])
+print("Result 2: ", result[net.number_of_layers - 2])
+
+
+# print (result[net.number_of_layers - 2])
+# net.learn([(5, 0)])
